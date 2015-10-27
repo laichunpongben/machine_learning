e6407100adb43e2ac2ce65af531ba4e674b0e3f3
laichunpongben <laichunpongben@gmail.com>
2015-10-28 02:50:49 +0800

    Update perceptron_learning_algorithm.py

--- perceptron_learning_algorithm.py
+++ perceptron_learning_algorithm.py
@@ -5,65 +5,54 @@ class TestCase:
     def __init__(self):
         self.n = 10
         self.d = 2
-        self.p0 = [random.uniform(-1,1) for x in range(2)]
-        self.p1 = [random.uniform(-1,1) for x in range(2)]
-        self.weights = [0 for x in range(self.d)]
+        self.p0 = [1] + [random.uniform(-1,1) for x in range(2)]
+        self.p1 = [1] + [random.uniform(-1,1) for x in range(2)]
+        self.weights = [0 for x in range(self.d+1)]
         self.training_set = [0 for x in range(self.n)]
         self.threshold = 0
         self.learning_rate = 1
 
     def target_function(self, point):
-        v1 = (self.p1[0] - self.p0[0], self.p1[1] - self.p0[1])
-        v2 = (self.p1[0] - point[0], self.p1[1] - point[1])
+        v1 = (self.p1[1] - self.p0[1], self.p1[2] - self.p0[2])
+        v2 = (self.p1[1] - point[1], self.p1[2] - point[2])
         xp = v1[0]*v2[1]-v1[1]*v2[0]
         return np.sign(xp)
 
     def initialize_training_set(self):
         for i in range(self.n):
-            x = tuple(random.uniform(-1,1) for x in range(self.d))
+            x = tuple([1] + [random.uniform(-1,1) for x in range(self.d)])
             y = self.target_function(x)
             self.training_set[i] = (x, y)
 
     def learn(self):
-        print(self.training_set)
         iteration = 0
-        while iteration < 100:
+        while iteration < 1000:
             iteration += 1
-            #print('-' * 50)
-            #print(iteration)
             error_count = 0
-            error_input_vectors = []
-            print(self.weights)
-            for input_vector, desired_output in self.training_set:
-                #print(['%.2f' % x for x in self.weights])
-                #print(input_vector)
-                result = np.sign(np.dot(input_vector, self.weights) - self.threshold)
-                #print('result: '+ str(result))
-                error = desired_output - result
-                #print('error: ' + str(error))
+            misclassified_x = {}
+            for x, y in self.training_set:
+                hx = np.sign(np.dot(x, self.weights))
+                error = y - hx
                 if error != 0:
                     error_count += 1
-                    error_input_vectors.append(input_vector)
+                    misclassified_x[x] = error
             if error_count > 0:
-                print('error count: ' + str(error_count))
-                update_vector = random.choice(error_input_vectors)
-                #print(update_vector)
-                y = self.target_function(update_vector)
-                self.weights = [w + self.learning_rate * y * x for w, x in zip(self.weights, update_vector)]
-                #print(error, error_count)
+                update_x = random.choice(list(misclassified_x.keys()))
+                epsilon = misclassified_x[update_x]
+                self.weights = [w + self.learning_rate * epsilon * x for w, x in zip(self.weights, update_x)]
             else:
                 break
-        #print('Iteration: ' + str(iteration))
         return iteration
 
-total_iteration = 0
-test_case_count = 0
-for x in range(1):
-    test_case_count += 1
-    test_case = TestCase()
-    test_case.initialize_training_set()
-    iteration = test_case.learn()
-    total_iteration += iteration
-    print(test_case.weights)
-    print(iteration)
-print(total_iteration / test_case_count)
+def learn_repeatedly(count):
+    total_iteration = 0
+    test_case_count = 0
+    for x in range(count):
+        test_case_count += 1
+        test_case = TestCase()
+        test_case.initialize_training_set()
+        iteration = test_case.learn()
+        total_iteration += iteration
+    print(total_iteration / test_case_count)
+    
+learn_repeatedly(50000)
